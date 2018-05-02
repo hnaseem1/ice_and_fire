3783b10054a6c2b8b6be4e31139bf07d32469fff
Hasan Naseem <hn92@live.com>
2018-05-02 15:11:32 -0400

    part 5 done

--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+
+
 class DragonTest < ActiveSupport::TestCase
 
   def test_that_dragon_shoots_fire
@@ -19,8 +21,8 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
-
+    young_dragon = build(:dragon, age: 2)
+    # binding.pry
     # not allowed to change the assert statement!
     skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
@@ -28,7 +30,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 20)
 
     # not allowed to change the assert statement!
     skip unless middle_dragon
@@ -37,7 +39,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 500)
 
     # not allowed to change the assert statement!
     skip unless ancient_dragon
@@ -46,8 +48,8 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_stranger_cannot_mount
     # use your knowledge of factories to make this test pass!
-    rider = nil
-    dragon = nil
+    rider = build(:rider, name: 'Aegon')
+    dragon = build(:dragon, name: 'Balerion', rider_id: 1)
 
     # not allowed to change the assert statement!
     skip unless rider && dragon
